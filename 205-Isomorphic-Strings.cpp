fea40f4da70d9dacbb0fcebac4919716d8c18b50
POOMESH-19 <149484652+POOMESH-19@users.noreply.github.com>
2024-12-23 11:19:48 +0530

    [205] [Time Beats: 100.00%] [Memory Beats: 75.62%] - LeetPush

---
+++ 205-Isomorphic-Strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        vector<int> s1(128, 0); 
+        vector<int> t1(128, 0); 
+        for (int i = 0; i < s.size(); ++i) {
+            if (s1[s[i]] != t1[t[i]]) {
+                return false;
+            }
+            s1[s[i]] = i + 1;
+            t1[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
