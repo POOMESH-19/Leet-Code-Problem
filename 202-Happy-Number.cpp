422b26482ea5546f9f92d01aa25c897944b40dc7
POOMESH-19 <149484652+POOMESH-19@users.noreply.github.com>
2024-12-23 11:59:46 +0530

    [202] [Time Beats: 100.00%] [Memory Beats: 81.97%] - LeetPush

--- 202-Happy-Number.cpp
+++ 202-Happy-Number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isHappy(int p) {
         if(p==1)
-        return true;
-        int s = 0;
+            return true;
+        int s=0;
         while (p > 1) {
             while (p > 0) {
                 int m = p % 10;
