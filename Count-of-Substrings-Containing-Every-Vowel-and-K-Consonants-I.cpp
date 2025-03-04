ee5604ca27050950b3000dc0018532fa5bf9ab34
Poomeshwaran L <149484652+POOMESH-19@users.noreply.github.com>
2025-03-04 15:36:45 +0530

    [3305] [Time Beats: 20.63%] [Memory Beats: 13.13%] - LeetPush

---
+++ Count-of-Substrings-Containing-Every-Vowel-and-K-Consonants-I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countOfSubstrings(string word, int k) 
+    {
+        unordered_set<char> vowel = {'a', 'e', 'i', 'o', 'u'};
+        int size = word.length();
+        int unique_counter = 0;
+        for (int i = 0; i < size; ++i) 
+        {
+            unordered_set<char> mpp;
+            int count = 0;
+            for (int j = i; j < size; ++j) 
+            {
+                if (vowel.find(word[j]) != vowel.end()) {
+                    mpp.insert(word[j]);
+                } else {
+                    count++;
+                }
+                if (mpp.size() == 5 && count == k) {
+                    unique_counter++;
+                }
+            }
+        }
+        return unique_counter;
+    }
+};
