e634497eb098f8fe06c0181efe9d8dd186034a17
Poomeshwaran L <149484652+POOMESH-19@users.noreply.github.com>
2025-09-22 14:57:18 +0530

    Time: 7 ms (61.97%), Space: 19.5 MB (57.05%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumDeletions(string s, int k) {
+        vector<int> f(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            f[s[i] - 'a']++;
+        }
+        vector<int> v;
+        for (int i = 0; i < 26; i++) {
+            if (f[i] != 0) {
+                v.push_back(f[i]);
+            }
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < v.size(); i++) {
+            int temp = 0;
+            for (int j = 0; j < v.size(); j++) {
+                if (v[j] < v[i]) {
+                    temp += v[j]; 
+                } else if (v[j] > v[i] + k) {
+                    temp += (v[j] - (v[i] + k)); 
+                }
+            }
+            ans = min(ans, temp);
+        }
+        return ans;
+    }
+};
